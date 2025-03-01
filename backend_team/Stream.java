e928b1a538d93c1c4c34bd9c4970fb8f4bb0a1c5
Admin <smartprakash809@gmail.com>
2025-03-01 15:45:23 +0530

    prakash:create arraylist

---
+++ backend_team/Stream.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Stream {
+
+    public static void main(String[] args) {
+        ArrayList<String> ob=new ArrayList<>();
+        ob.add("prakash");
+        ob.add("akash");
+        ob.add("ram");
+    }
+}
